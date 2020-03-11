c07b1d1c28161cbff524ee95dcc861b93cc0cd2c
MadKimchi <minkim.dev@gmail.com>
2020-03-10 23:54:08 -0400

    add services (moved from the author`s react-boilerplate repo

---
+++ packages/core/src/services/base-injector.service.ts
@@ -0,0 +1,37 @@
+import { AuthService } from './auth.service';
+import { MessageService } from './message.service';
+import { ErrorService } from './error.service';
+import { HttpClient } from '../https/http-client';
+
+export class BaseInjectorService {
+
+  private _errorService: ErrorService;
+  private _httpClient: HttpClient;
+
+  constructor() {
+    this._errorService = new ErrorService();
+    this._httpClient = new HttpClient(this._errorService);
+  }
+
+  private _authService!: AuthService;
+  public get authService(): AuthService {
+    if (!this._authService) {
+      this._authService = new AuthService(this._httpClient);
+    }
+
+    return this._authService;
+  }
+
+  private _messageService!: MessageService;
+  public get messageService(): MessageService {
+    if (!this._messageService) {
+      this._messageService = new MessageService();
+    }
+
+    return this._messageService;
+  }
+
+  public get errorService(): ErrorService {
+    return this._errorService;
+  }
+}
--- packages/core/src/services/index.ts
+++ packages/core/src/services/index.ts
@@ -1,1 +1,4 @@
 export * from './error.service';
+export * from './auth.service';
+export * from './message.service';
+export * from './base-injector.service';
